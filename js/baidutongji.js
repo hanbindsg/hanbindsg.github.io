c32df739450e77d2f87060a93648cd39e8e1f336
hanbindsg <admin@acgzj.cn>
2016-08-30 22:10:13 +0800

    Add files via upload

---
+++ js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?18a760886d79903378f921e08b44ae3b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
